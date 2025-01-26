19035b2ff3ee8da60f9573f47fbb2fc950aa6be9
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2025-01-26 15:42:48 +0300

    Добавлен компонент темы.

---
+++ Client/src/theme/Theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const Theme = createTheme(
+    {
+        typography: {
+            fontFamily: 'Roboto, Arial, sans-serif',
+        },
+        colorSchemes: {
+            dark: true,
+        },
+    }
+);
+
+export default Theme;
