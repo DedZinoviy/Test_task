36de1227eb9e80d67dc0248b5700bbc5a1130015
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2025-01-26 15:55:14 +0300

    Переработана логика работы с хранилищем redux.

--- Client/src/redux/CommentSlice.ts
+++ Client/src/redux/CommentSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Comment } from '../types/Comment';
+import { Comment } from '../types';
 
 interface CommentsState {
   data: Comment[];
--- Client/src/redux/store.ts
+++ Client/src/redux/store.ts
@@ -9,5 +9,9 @@ export const store = configureStore({
 });
 
 // Типы для использования в проекте
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+
+// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+export type AppStore = typeof store
