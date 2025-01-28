3fde17b3acdc87f6fdc30e414b63edd4a3858dae
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2025-01-28 11:10:04 +0300

    Убраны лишние комментарии из хранилища redux

--- Client/src/redux/store.ts
+++ Client/src/redux/store.ts
@@ -10,8 +10,6 @@ export const store = configureStore({
 
 // Типы для использования в проекте
 
-// Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 export type AppStore = typeof store
